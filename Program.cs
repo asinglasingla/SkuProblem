ea4d6ec628f9100eab922c548ff4ffa88004a2b2
asinglasingla <77638775+asinglasingla@users.noreply.github.com>
2021-01-18 22:45:38 -0800

    added program cs file ,
    not able to push initial files to github so just uploaded the files and i missed this program cs in first commit

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SkuProblem
+{
+    /// <summary>
+    /// Sole purpose of this class is to configure cart and promotions as per your need.
+    /// If you want to run test cases or the given scenarion just visit PromotionHandlerTest.cs
+    /// </summary>
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sku present in the cart
+            List<Sku> cartItems = new List<Sku> {
+            new Sku{SkuId = 'A', UnitPrice = 50, Quantity = 3},
+            new Sku{SkuId = 'B', UnitPrice = 30, Quantity = 5},
+            new Sku{SkuId = 'C', UnitPrice = 20, Quantity = 1},
+            new Sku{SkuId = 'D', UnitPrice = 30, Quantity = 1}
+            };
+
+            //Confifured cart based on scenarios
+            Cart cart = new Cart();
+            cart.AddToCart(cartItems);
+
+
+            //Configuring what all SkuId's involved in promotion. These promotions can also be configured based on the 
+            //the values in database. For e.g if we add all these sku details in DB then these can be generated automatically
+            Promotion promotion = new Promotion(130)
+            {
+                SkuInvolved = new List<Sku> { new Sku { SkuId = 'A', Quantity = 3 } },
+                Successor = new Promotion(45)
+                {
+                    SkuInvolved = new List<Sku> { new Sku { SkuId = 'B', Quantity = 2 } },
+                    Successor = new Promotion(30)
+                    {
+                        SkuInvolved = new List<Sku> { 
+                            new Sku { SkuId = 'C', Quantity = 2 } ,
+                            new Sku { SkuId = 'D', Quantity = 1 } 
+                        }
+                    }
+                }
+            };
+
+            //Registered Promotions with the Promotion handler which will calculate the promotion for the cart items
+            PromotionHandler promotionHandler = new PromotionHandler();
+            promotionHandler.RegisterPromotion(promotion);
+            promotionHandler.Apply(cart);
+
+            Console.WriteLine(promotionHandler.TotalValueAfterPromotion);
+        }
+    }
+}
