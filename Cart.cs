1edfdbd18204a4bd95435f4c1fa908db0251cea3
asinglasingla <77638775+asinglasingla@users.noreply.github.com>
2021-01-18 22:37:06 -0800

    created solution and added cart to maintain Sku
    created solution and added cart to maintain Sku

---
+++ Cart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SkuProblem
+{
+    public class Cart
+    {
+        public List<Sku> CartItems { get; private set; }
+        public Cart() {
+            CartItems = new List<Sku>();
+        }
+
+        public void AddToCart(Sku sku)
+        {
+            CartItems.Add(sku);
+        }
+
+        public void AddToCart(IEnumerable<Sku> skus)
+        {
+            CartItems.AddRange(skus);
+        }
+    }
+}
---
+++ Sku.cs
@@ -0,0 +1,9 @@
+﻿namespace SkuProblem
+{
+   public class Sku
+    {
+        public char SkuId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
